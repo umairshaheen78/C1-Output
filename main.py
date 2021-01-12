4490a0f523c877799ec07f6640d0041157247d77
Umair Shaheen <109942@eesd.org>
2021-01-12 00:08:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a comments. Comments are not ran by the compiler. 
+# Output: information that the computer prints out, what you see printed on the right hand side
+
+print("Hello World!")
+
+# Print out your name, grade, favorite book, and superhero!
+
+print("My name is Umair Shaheen. I am in 7th grade. My favorite book is the Series of Unfortunate Events! My favorite superhero is probably Batman.")
+
+print("Hello")
+print("World")
+
+#Concatenation: add 2 strings together using '+'
+
+print("Hello" + "World")
